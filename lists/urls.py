58769966e89efc26110472d511cbbe4a0cb270a0
Man Ly <7685085+man1y@users.noreply.github.com>
2024-05-15 21:39:32 -0700

    Separated URL configuration for lists app to remove duplication

--- lists/urls.py
+++ lists/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for superlists project.
+URL configuration for lists app.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -14,12 +14,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import include, re_path, path
-from lists import views as list_views
-from lists import urls as list_urls
+from django.urls import re_path, path
+from lists import views
 
 urlpatterns = [
-    re_path(r'^$', list_views.home_page, name='home'),
-    re_path(r'^lists/', include(list_urls)),
+    path('new', views.new_list, name='new_list'),
+    re_path(r'^(\d+)/$', views.view_list, name='view_list'),
+    re_path(r'^(\d+)/add_item$', views.add_item, name='add_item'),
 ]
