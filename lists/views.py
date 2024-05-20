83c0c79c5802c9a9073a331bda78678327863b81
Man Ly <7685085+man1y@users.noreply.github.com>
2024-05-19 23:47:41 -0700

    Refactor list view to handle new item POSTs

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse
+from django.http import HttpRequest, HttpResponse
 from django.core.exceptions import ValidationError
 
 from lists.models import Item, List
@@ -8,8 +8,12 @@ from lists.models import Item, List
 def home_page(request) -> HttpResponse:
     return render(request, 'home.html')
 
-def view_list(request, list_id) -> HttpResponse:
+def view_list(request: HttpRequest, list_id) -> HttpResponse:
     list_ = List.objects.get(id=list_id)
+    if request.method == 'POST':
+        Item.objects.create(text=request.POST['item_text'], list=list_)
+        return redirect(f'/lists/{list_.id}/')
+
     return render(request, 'list.html', {'list': list_})
 
 def new_list(request) -> HttpResponse:
@@ -22,11 +26,5 @@ def new_list(request) -> HttpResponse:
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, 'home.html', {"error": error})
-
-    return redirect(f'/lists/{list_.id}/')
-
-def add_item(request, list_id) -> HttpResponse:
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
 
     return redirect(f'/lists/{list_.id}/')
